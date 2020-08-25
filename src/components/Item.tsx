f9e948b5e8a3456ac97b98012fc88b778f188a66
Rani <ranihorev@gmail.com>
2020-08-25 14:55:44 -0700

    close on click prop

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,14 +1,14 @@
-import React, { Component, ReactNode } from 'react';
-import PropTypes from 'prop-types';
-import cx from 'classnames';
+import React, { Component, ReactNode } from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 
-import { styles } from '../utils/styles';
+import { styles } from "../utils/styles";
 import {
   MenuItemEventHandler,
   TriggerEvent,
   StyleProps,
-  InternalProps
-} from '../types';
+  InternalProps,
+} from "../types";
 
 export interface ItemProps extends StyleProps, InternalProps {
   /**
@@ -25,6 +25,11 @@ export interface ItemProps extends StyleProps, InternalProps {
    * Disable or not the `Item`. If a function is used, a boolean must be returned
    */
   disabled: boolean | ((args: MenuItemEventHandler) => boolean);
+
+  /**
+   * Whether the `Menu` should close or not from a click on the `Item`. If a function is used, a boolean must be returned
+   */
+  closeOnClick?: boolean | ((args: MenuItemEventHandler) => boolean);
 
   /**
    * Callback when the current `Item` is clicked. The callback give you access to the current event and also the data passed
@@ -45,43 +50,58 @@ class Item extends Component<ItemProps> {
     nativeEvent: PropTypes.object,
     propsFromTrigger: PropTypes.object,
     className: PropTypes.string,
-    style: PropTypes.object
+    style: PropTypes.object,
+    closeOnClick: PropTypes.bool,
   };
 
   static defaultProps = {
     disabled: false,
-    onClick: noop
+    onClick: noop,
+    closeOnClick: true
   };
 
   isDisabled: boolean;
+  stayOpenOnClick: boolean;
 
   constructor(props: ItemProps) {
     super(props);
-    const { disabled, nativeEvent, propsFromTrigger, data } = this.props;
+    const { disabled, nativeEvent, propsFromTrigger, data, closeOnClick } = this.props;
 
     this.isDisabled =
-      typeof disabled === 'function'
+      typeof disabled === "function"
         ? disabled({
             event: nativeEvent as TriggerEvent,
-            props: { ...propsFromTrigger, ...data }
+            props: { ...propsFromTrigger, ...data },
           })
         : disabled;
+
+    this.stayOpenOnClick = this.isDisabled ||
+    !(typeof closeOnClick === 'function'
+      ? closeOnClick({
+        event: nativeEvent as TriggerEvent,
+        props: { ...propsFromTrigger, ...data }
+      }) : closeOnClick);
   }
+  
 
   handleClick = (e: React.MouseEvent) => {
-    this.isDisabled
-      ? e.stopPropagation()
-      : this.props.onClick({
-          event: this.props.nativeEvent as TriggerEvent,
-          props: { ...this.props.propsFromTrigger, ...this.props.data }
-        });
+    if (this.stayOpenOnClick) {
+      e.stopPropagation();
+    }
+
+    if (!this.isDisabled) {
+      this.props.onClick({
+        event: this.props.nativeEvent as TriggerEvent,
+        props: { ...this.props.propsFromTrigger, ...this.props.data }
+      });
+    }
   };
 
   render() {
     const { className, style, children } = this.props;
 
     const cssClasses = cx(styles.item, className, {
-      [`${styles.itemDisabled}`]: this.isDisabled
+      [`${styles.itemDisabled}`]: this.isDisabled,
     });
 
     return (
